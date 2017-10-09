9da479257617dc13cc26255bc8c9b35f837a5812
Pancho <panchenko923@gmail.com>
2017-10-10 01:32:58 +0300

    some refs

--- src/RefsAndDom.js
+++ src/RefsAndDom.js
@@ -7,6 +7,7 @@ class RefsAndDom extends React.Component {
 		return (
 			<div>
 				<AutoFocus />
+				<GetFourth />
 			</div>
 		);
 
@@ -28,7 +29,7 @@ class CustomInput extends React.Component {
 	render () {
 		return (
 			<div>
-				<input type="text" ref={thisInput => this.textInput = thisInput} />
+				<input type="text" ref={input => this.textInput = input} />
 				<button onClick={this.focus}>Focus the text input</button>
 			</div>
 		)
@@ -38,15 +39,41 @@ class CustomInput extends React.Component {
 class AutoFocus extends React.Component {
 
 	componentDidMount() {
-		this.textInput.focus();
+		this.currentInput.focus();
 	}
 
 	render () {
 		return (
-			<CustomInput ref={thisInput => this.textInput = thisInput} />
+			<CustomInput ref={input => this.currentInput = input} />
 		);
 	}
 
+}
+
+class Divs extends React.Component {
+	render () {
+		return (
+			<div>
+				<div ref={div => this.firstDiv = div}>1</div>
+				<div ref={div => this.secondDiv = div}>2</div>
+				<div ref={div => this.thirdDiv = div}>3</div>
+				<div ref={div => this.fourthDiv = div}>4</div>
+			</div>
+		);
+	}
+}
+
+class GetFourth extends React.Component {
+
+	componentDidMount () {
+		console.log(this.divs.fourthDiv); // <div>4</div>
+	}
+
+	render () {
+		return (
+			<Divs ref={div => this.divs = div} />
+		);
+	}
 }
 
 export default RefsAndDom;
